65216699ec23dcc5be520526999a772829372eac
John San Martin <jhonsanmartin11@gmail.com>
2024-07-13 09:58:54 -0500

    Listo

--- movies/models.py
+++ movies/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Movie(models.Model):
+    title = models.CharField(max_length=50, null=False)
+    genre = models.CharField(max_length=50, null=False)
+    director = models.CharField(max_length=50, null=False)
+    year = models.IntegerField(null=False)
+    sinopsis = models.TextField(null=False)
+    
+    def __str__(self) -> str:
+        return self.title
